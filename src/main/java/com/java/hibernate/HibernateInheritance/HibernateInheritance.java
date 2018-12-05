2ad5357c8f1793261e30016376c413cb3aa0e5bc
ABJ2018 <amitjadhav2380@gmail.com>
2018-12-05 13:53:47 +0530

    msg

---
+++ src/main/java/com/java/hibernate/HibernateInheritance/HibernateInheritance.java
@@ -0,0 +1,11 @@
+package com.java.hibernate.HibernateInheritance;
+
+public class HibernateInheritance 
+{
+public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println("Word");
+	System.out.println("wellcome");
+}
+
+}
